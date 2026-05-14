8249786b84954ff032b7950c08504f6320131d5d
hellohim <elohim.mbu@gmail.com>
2026-05-14 18:53:10 +0100

    feat: add dynamic table headers
    Generate table headers dynamically from book properties to enable
    flexibility and scalability. Table will automatically adjust when
    new properties are added to the Book object.

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const myLibrary = [];
+const myLibrary = []
 
-function Book(name, author, readStatus) {
-  this.name = name;
-  this.author = author;
-  this.readStatus = readStatus;
+function Book(name, author, status) {
+  this.name = name
+  this.author = author
+  this.status = status
 }
 
-function addBookToLibrary(name, author, readStatus) {
-  const newBook = new Book(name, author, readStatus);
-  let uuid = crypto.randomUUID();
-  newBook.id = uuid;
-  myLibrary.push(newBook);
+function addBookToLibrary(name, author, status) {
+  const newBook = new Book(name, author, status)
+  let uuid = crypto.randomUUID()
+  newBook.id = uuid
+  myLibrary.push(newBook)
+  getColumnHeaders(myLibrary)
 }
+
+function getColumnHeaders(myLibrary) {
+  const table = document.getElementById("libraryTable")
+
+  // Get the thead element
+  const thead = table.tHead
+
+  // Insert a row into the thead(index 0 = first row)
+  const newHeaderRow = table.insertRow(0)
+
+  // Add cells to row by through thourhg key array and using insertCell()
+  Object.keys(myLibrary[0]).forEach((element, index) => {
+    const cell = newHeaderRow.insertCell(index)
+    cell.textContent = element.charAt(0).toUpperCase() + element.slice(1)
+  })
+}
+
+// Test
+addBookToLibrary("Harry Potter & the Curse of the Honoured One", "Gojo")
